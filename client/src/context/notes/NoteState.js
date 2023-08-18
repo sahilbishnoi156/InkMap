5339679b4711c1a56d37685e012737908247a4de
sahilbishnoi156 <sahilbishnoi156@gmail.com>
2023-08-18 15:35:50 +0530

    fixing ports

--- client/src/context/notes/NoteState.js
+++ client/src/context/notes/NoteState.js
@@ -4,7 +4,7 @@ import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 export default function NoteState(props) {
-  const host = "http://localhost:5000";
+  const host = "https://ink-map-backend-nsehzfqac-sskkpoonia-gmailcom.vercel.app/";
   const notesInitial = [];
   const [notes, setNotes] = useState(notesInitial);
   const [userDate, setUserDate] = useState('')
